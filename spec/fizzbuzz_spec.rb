f86c39fab924736f664276ca38672142d694bf07
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-12 15:24:02 +0000

    testing finished, appears to be fully working

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -22,4 +22,7 @@ describe 'fizzbuzz' do
   it 'returns "2" when passed 2' do
     expect(2.fizzbuzz).to eq "2"
   end
+  it 'returns "fizz" when passed -3' do
+    expect(-3.fizzbuzz).to eq "fizz"
+  end
 end
