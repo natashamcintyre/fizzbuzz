244f7c7014c6111f8d19f7437850787b457ba532
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-12 13:49:44 +0000

    pass for 3, 1 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,6 +3,8 @@ class Integer
   def fizzbuzz
     if self == 3
       "fizz"
+    elsif self == 5
+      "buzz"
     else
       "1"
     end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(3.fizzbuzz).to eq "fizz"
   end
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq "buzz"
+  end
 end
